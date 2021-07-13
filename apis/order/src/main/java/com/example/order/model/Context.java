ed90d752ffd34ec5aa822a64c23a29a070829511
exfirewall <zpqls321@naver.com>
2021-07-13 11:40:49 +0900

    add Context domain

---
+++ apis/order/src/main/java/com/example/order/model/Context.java
@@ -0,0 +1,24 @@
+package com.example.order.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Context implements Serializable {
+    private static final long serialVersionVID = 7519387928670129413L;
+
+    @JsonProperty("process_id")
+    private String processId;
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+}
